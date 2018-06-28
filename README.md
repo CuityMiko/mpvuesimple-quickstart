8ac78e1a30affb7d7410a86f691f75650b114f67
blackjack0v0 <yihongwy1@163.com>
2018-06-29 00:24:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# mpvue-quickstart
+
+> fork 自 [vuejs-templates/webpack](https://github.com/vuejs-templates/webpack) 修改而来
+
+## 基本用法
+
+``` bash
+$ npm install -g vue-cli
+$ vue init F-loat/mpvue-quickstart my-project
+$ cd my-project
+$ npm install
+$ npm run dev
+```
+
+## 主要特性
+
+* 移除无效依赖
+* 集中配置页面信息
+* 合理化目录结构
+* 支持新增页面热更新
+* 自动清理失效文件
+* 配置文件模块化
+
+## 详细文档请查阅
+
+* [quickstart](http://mpvue.com/mpvue/quickstart)
+* [mpvue-entry](https://github.com/F-loat/mpvue-entry)
---
+++ template/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from '@/App'
+import store from '@/store'
+
+Vue.config.productionTip = false
+
+import IboxPlugin from '@/plugins/ibox'
+Vue.use(IboxPlugin)
+
+const app = new Vue({
+  store,
+  ...App
+})
+
+app.$mount()
+
+export default {
+  config: {
+    pages: [],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
